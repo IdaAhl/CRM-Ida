fbab3f1b8440c44b959788ae8a28aadc8abfb3cd
Ida Ahlström <ida.ahlstrom@collectorbank.se>
2018-04-09 09:33:48 +0200

    Add project files.

---
+++ CRM-Ida/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRM_Ida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
